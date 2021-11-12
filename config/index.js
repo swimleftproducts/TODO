65773e2cf7591f9c33092ba3c40ff4abae1e3f29
Eric Harvey <swimleft@gmail.com>
2021-11-12 08:52:20 -0800

    linkupdate

---
+++ config/index.js
@@ -0,0 +1,10 @@
+
+
+module.exports={
+    getDevDbConnectionString:function(){     
+        if(process.env.NODE_ENV ==="production"){
+        return `mongodb+srv://${process.env.DBUNAME}:${process.env.DBPWD}@cluster0.d6dt5.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`}else{
+        return `mongodb+srv://${process.env.DBUNAME}:${process.env.DBPWD}@cluster0.d6dt5.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+    }
+    }
+}
